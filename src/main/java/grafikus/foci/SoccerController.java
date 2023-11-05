1b60a021d8ee0a46b6f9803e6cf6b66e7314ef1b
bdcs02 <126778967+bdcs02@users.noreply.github.com>
2023-11-05 21:08:36 +0100

    párhuzamos szálkezelés fullos

--- src/main/java/grafikus/foci/SoccerController.java
+++ src/main/java/grafikus/foci/SoccerController.java
@@ -1,18 +1,24 @@
 package grafikus.foci;
 
 import grafikus.foci.model.SoccersModel;
+import grafikus.foci.controller.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import grafikus.foci.controller.menuDatabaseController;
 import grafikus.foci.controller.menuRestController;
 import grafikus.foci.controller.*;
 
+import java.io.IOException;
 import java.util.List;
 
 public class SoccerController {
@@ -92,6 +98,7 @@ public class SoccerController {
         tableView.getItems().addAll(lista);
 
         t.commit();
+        session.close();
     }
 
 
@@ -102,16 +109,40 @@ public class SoccerController {
         tableView.setManaged(true);
         tableView.setVisible(true);
         tableView.getItems().clear();
+
         String searchText = searchField.getText();
-        String selectedFilter = filterComboBox.getValue();
         boolean isHungarian = radioButton.isSelected();
         boolean isForeign = checkBox.isSelected();
 
         Session session = SoccerApplication.getSession();
         Transaction t = session.beginTransaction();
 
+        // Készítse el a HQL lekérdezést csak azokkal a feltételekkel, amelyeket a felhasználó megadott
+        String hql = "FROM SoccersModel WHERE 1=1"; // Alapértelmezett feltétel
+
+        if (!searchText.isEmpty()) {
+            hql += " AND (id LIKE :searchText OR name LIKE :searchText OR value LIKE :searchText)";
+        }
+
+        if (isForeign) {
+            hql += " AND kulfoldi = true";
+        }
+
+        org.hibernate.query.Query<SoccersModel> query = session.createQuery(hql, SoccersModel.class);
+
+        if (!searchText.isEmpty()) {
+            query.setParameter("searchText", "%" + searchText + "%");
+        }
+
+        List<SoccersModel> lista = query
+                .setMaxResults(10)
+                .list();
+
+        tableView.getItems().addAll(lista);
 
         t.commit();
+        session.close();
+
     }
     public void menuDatabaseCreate(ActionEvent actionEvent) {
         ElemekTörlése();
@@ -133,7 +164,24 @@ public class SoccerController {
     public void menuDataMoreA2(ActionEvent actionEvent) {
     }
 
-    public void menuParalell(ActionEvent actionEvent) {
+    public void menuParalell(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("paralell.fxml"));
+        Parent root = loader.load();
+        ParalellController controller = loader.getController();
+
+        Scene scene = new Scene(root);
+
+        // Hozzon létre egy új Stage objektumot (ablak)
+        Stage stage = new Stage();
+
+        // Állítsa be az ablak címét
+        stage.setTitle("Párhuzamos Végrehajtás");
+
+        // Állítsa be az ablak Scene-jét
+        stage.setScene(scene);
+
+        // Mutassa be az ablakot
+        stage.show();
     }
 
     public void menuStream(ActionEvent actionEvent) {
